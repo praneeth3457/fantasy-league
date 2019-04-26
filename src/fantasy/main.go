33debb03b85e6dd42519df14b17a203c58216bec
CHANDRAKANTHREDDY93 <praneeth7516@gmail.com>
2019-04-25 23:24:22 -0400

    Created save match players API

--- src/fantasy/main.go
+++ src/fantasy/main.go
@@ -42,6 +42,7 @@ func main() {
 	r.Handle("/api/match/create", authorization.IsAuthorized(routes.CreateMatch, constant.UserAdmin)).Methods("POST")
 	r.Handle("/api/match/getAllMatches", authorization.IsAuthorized(routes.GetAllMatches, constant.UserAny)).Methods("GET")
 	r.Handle("/api/match/otherMatchDetails", authorization.IsAuthorized(routes.OtherMatchDetails, constant.UserAny)).Methods("POST")
+	r.Handle("/api/match/saveOtherMatchDetails", authorization.IsAuthorized(routes.SaveOtherMatchDetails, constant.UserAny)).Methods("POST")
 	//usersPlayers routes
 	r.Handle("/api/usersPlayers/createAvailability", authorization.IsAuthorized(routes.CreateAvailability, constant.UserAuthor)).Methods("GET")
 	r.Handle("/api/usersPlayers/getAvailability", authorization.IsAuthorized(routes.GetAvailability, constant.UserAny)).Methods("POST")
--- src/fantasy/routes/matches.go
+++ src/fantasy/routes/matches.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"fmt"
+	"strconv"
 
 	"db"
 	model "models"
@@ -125,4 +126,116 @@ func OtherMatchDetails(w http.ResponseWriter, r *http.Request) {
 	
 	json.NewEncoder(w).Encode(model.Response3{Success: true, Message: otherDetail})
 
+}
+
+/*
+	Checking if all players are unique
+	Checking if match is in active mode(edit mode)
+	Checking if other row exist
+	If exist modify the existing other row
+	Also update the players availability by reducing the available field & by adding the removed player
+	Else create new other row
+	Also, update the players availability by reducing the available field
+*/
+func SaveOtherMatchDetails(w http.ResponseWriter, r *http.Request) {
+	var (
+		other model.Other
+		otherDb model.Other
+		response model.Response
+		status int
+	)
+	_ = json.NewDecoder(r.Body).Decode(&other)
+
+	//Checking if all players are unique
+	if other.Captain == other.MVBA || other.Captain == other.MVBO || other.Captain == other.MVAR || other.MVBA == other.MVBO || other.MVBA == other.MVAR || other.MVBO == other.MVAR{
+		response = model.Response{Success: false, Message: "All players should be unique"}
+		json.NewEncoder(w).Encode(response)
+		return
+	}
+
+	//Checking if match is in active mode(edit mode)
+	err := database.Db.QueryRow("SELECT Status FROM matchesTbl WHERE MID=@MID", sql.Named("MID", other.MID)).Scan(&status)
+	if err != nil {
+		response = model.Response{Success: false, Message: "Invalid match."}
+		json.NewEncoder(w).Encode(response)
+		return
+	}
+	if status == 2 {
+		response = model.Response{Success: false, Message: "Cannot make changes. Match is locked."}
+		json.NewEncoder(w).Encode(response)
+		return
+	} 
+	if status == 3 {
+		response = model.Response{Success: false, Message: "Cannot make changes. Match is completed."}
+		json.NewEncoder(w).Encode(response)
+		return
+	}
+
+	//Checking if other row exist
+	tx, _ := database.Db.Begin()
+	err2 := tx.QueryRow("SELECT OID, Captain, MVBA, MVBO, MVAR FROM othersTbl WHERE MID=@MID AND UID=@UID", sql.Named("MID", other.MID), sql.Named("UID", other.UID)).Scan(&otherDb.OID, &otherDb.Captain, &otherDb.MVBA, &otherDb.MVBO, &otherDb.MVAR)
+	if err2 != nil {
+		//Else create new other row
+		stmt, err3 := tx.Prepare("INSERT INTO othersTbl(Captain,MVBA,MVBO,MVAR,UID,MID) VALUES(@Captain, @MVBA, @MVBO, @MVAR, @UID, @MID)")
+		_, err3 = stmt.Exec(sql.Named("Captain", other.Captain), sql.Named("MVBA", other.MVBA), sql.Named("MVBO", other.MVBO), sql.Named("MVAR", other.MVAR), sql.Named("UID", other.UID), sql.Named("MID", other.MID))
+		if err3 != nil {
+			tx.Rollback()
+			response = model.Response{Success: false, Message: "Error in saving data."}
+			json.NewEncoder(w).Encode(response)
+			return
+		}
+
+		//Also, update the players availability by reducing the available field
+		ids := strconv.Itoa(other.Captain) + `, ` + strconv.Itoa(other.MVBA) + `, ` + strconv.Itoa(other.MVBO) + `, ` + strconv.Itoa(other.MVAR)
+		updateAvailability := fmt.Sprintf(`UPDATE availabilityTbl SET Available = Available - 1 WHERE UID=@UID AND PID IN (%s)`, ids)
+		stmt, err4 := tx.Prepare(updateAvailability)
+		_, err4 = stmt.Exec(sql.Named("UID", other.UID))
+		if err4 != nil {
+			tx.Rollback()
+			response = model.Response{Success: false, Message: err4.Error()}
+			json.NewEncoder(w).Encode(response)
+			return
+		}
+	}
+
+	//Also update the players availability by adding the removed player
+	ids2 := strconv.Itoa(otherDb.Captain) + `, ` + strconv.Itoa(otherDb.MVBA) + `, ` + strconv.Itoa(otherDb.MVBO) + `, ` + strconv.Itoa(otherDb.MVAR)
+	updateAvailability := fmt.Sprintf(`UPDATE availabilityTbl SET Available = Available + 1 WHERE UID=@UID AND PID IN (%s)`, ids2)
+	stmt, err5 := tx.Prepare(updateAvailability)
+	_, err5 = stmt.Exec(sql.Named("UID", other.UID))
+	if err5 != nil {
+		tx.Rollback()
+		log.Println("err5" + err5.Error())
+		response = model.Response{Success: false, Message: err5.Error()}
+		json.NewEncoder(w).Encode(response)
+		return
+	}
+
+	//If exist modify the existing other row
+	stmt, err6 := tx.Prepare("UPDATE othersTbl SET Captain=@Captain, MVBA=@MVBA, MVBO=@MVBO, MVAR=@MVAR WHERE OID=@OID")
+	_, err6 = stmt.Exec(sql.Named("Captain", other.Captain), sql.Named("MVBA", other.MVBA), sql.Named("MVBO", other.MVBO), sql.Named("MVAR", other.MVAR), sql.Named("OID", otherDb.OID))
+	if err6 != nil {
+		tx.Rollback()
+		log.Println("err6" + err6.Error())
+		response = model.Response{Success: false, Message: err6.Error()}
+		json.NewEncoder(w).Encode(response)
+		return
+	}
+
+	//Also update the players availability by reducing the available field
+	ids3 := strconv.Itoa(other.Captain) + `, ` + strconv.Itoa(other.MVBA) + `, ` + strconv.Itoa(other.MVBO) + `, ` + strconv.Itoa(other.MVAR)
+	updateAvailability3 := fmt.Sprintf(`UPDATE availabilityTbl SET Available = Available - 1 WHERE UID=@UID AND PID IN (%s)`, ids3)
+	stmt, err7 := tx.Prepare(updateAvailability3)
+	_, err7 = stmt.Exec(sql.Named("UID", other.UID))
+	if err7 != nil {
+		tx.Rollback()
+		log.Println("err7" + err7.Error())
+		response = model.Response{Success: false, Message: err7.Error()}
+		json.NewEncoder(w).Encode(response)
+		return
+	}
+
+	tx.Commit()
+	response = model.Response{Success: true, Message: "Successfully saved."}
+	json.NewEncoder(w).Encode(response)
 }
